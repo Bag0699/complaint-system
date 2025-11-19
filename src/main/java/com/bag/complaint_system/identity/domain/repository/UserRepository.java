17f64010fe5116ebae780add8659ca0687acf3cf
Brayam Ayala <b.ayalaga06@gmail.com>
2025-11-19 12:53:12 -0500

    Add `getAllUsers` endpoint to `UserController` and implement supporting repository method

--- src/main/java/com/bag/complaint_system/identity/domain/repository/UserRepository.java
+++ src/main/java/com/bag/complaint_system/identity/domain/repository/UserRepository.java
@@ -8,20 +8,21 @@ import java.util.Optional;
 
 public interface UserRepository {
 
-    User save(User user);
+  User save(User user);
 
-    Optional<User> findById(Long id);
+  Optional<User> findById(Long id);
 
-    Optional<User> findByEmail(Email email);
+  Optional<User> findByEmail(Email email);
 
-    boolean existsByEmail(Email email);
+  boolean existsByEmail(Email email);
 
-    List<User> findAllActive();
+  List<User> findAllActive();
 
-    List<User> findAllByRole(String role);
+  List<User> findAllByRole(String role);
 
-    List<User> findAllById(List<Long> ids);
+  List<User> findAllById(List<Long> ids);
 
-    void deleteById(Long id);
+  void deleteById(Long id);
 
+  List<User> findAll();
 }
